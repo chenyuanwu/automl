2cf2cca6c3eab87b704017ad69cfc285f7b1c282
chenyuanwu <1404059145@qq.com>
2020-04-22 13:27:30 +0800

    [crawler]

--- crawler.py
+++ crawler.py
@@ -5,9 +5,9 @@ import re
 import os
 
 # starturl = "https://pantheon.stanford.edu/measurements/node/?node=any&direction=any&link=any&scenario=any&year=any&month=any&page=1"
-starturl = "https://pantheon.stanford.edu/measurements/cloud/"
-datapath = "/home/chenyunw/automl/cloud2cloud"
-baseurl = "https://pantheon.stanford.edu/measurements/cloud/"
+starturl = "https://pantheon.stanford.edu/measurements/node/"
+datapath = "/home/chenyunw/automl/node2cloud"
+baseurl = "https://pantheon.stanford.edu/measurements/node/"
 
 html = urlopen(starturl).read().decode('utf-8')
 soup = BeautifulSoup(html, features='lxml')
@@ -16,7 +16,7 @@ for i in range(50000):
     current_page_item = soup.find_all('li', attrs={"class":"page-item active"})[0]
     current_page_index = re.findall(r'[0-9]+', current_page_item.a.get_text())[0]
     os.mkdir(os.path.join(datapath, 'test' + current_page_index))
-    download_names = ["Full report"]#, "Performance data", "Raw logs", "Raw logs with UIDs"]
+    download_names = ["Full report", "Performance data", "Raw logs", "Raw logs with UIDs"]
     download_tags = soup.find_all('a', text=download_names)
     for t in download_tags:
         urlretrieve(t['href'], os.path.join(datapath, 'test' + current_page_index, t['href'].split("/")[-1]))
